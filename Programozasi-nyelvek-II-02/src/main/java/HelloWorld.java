a58e5fc43861a70217986376e3d4d882a062d28d
Fábián Gábor <info@fabiangabor.com>
2020-02-23 15:18:09 +0100

    check if args empty

--- Programozasi-nyelvek-II-02/src/main/java/HelloWorld.java
+++ Programozasi-nyelvek-II-02/src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main (String[] args)
     {
+        if (args.length != 0)
+            System.out.println(args[0]);
         System.out.println("Hello Kitty");
         for (int i=2; i<=20; i+=2)
             System.out.println(i);
