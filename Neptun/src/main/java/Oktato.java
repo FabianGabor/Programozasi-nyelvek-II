6062bb71b96ce8cc8eac8b40ff5f3dbea0fc1614
Fábián Gábor <info@fabiangabor.com>
2020-03-08 14:41:19 +0100

    class Oktato

--- Neptun/src/main/java/Oktato.java
+++ Neptun/src/main/java/Oktato.java
@@ -1,2 +1,26 @@
-public class Oktato {
+public class Oktato extends Ember {
+    private String neptunKod;
+    Hallgato hallgatok[] = new Hallgato[10];
+
+    public Oktato(String nev, int szuletesiEv, String neptunKod, Hallgato[] hallgatok) {
+        super(nev, szuletesiEv);
+        this.neptunKod = neptunKod;
+        this.hallgatok = hallgatok;
+    }
+
+    public String getNeptunKod() {
+        return neptunKod;
+    }
+
+    public void setNeptunKod(String neptunKod) {
+        this.neptunKod = neptunKod;
+    }
+
+    public Hallgato[] getHallgatok() {
+        return hallgatok;
+    }
+
+    public void setHallgatok(Hallgato[] hallgatok) {
+        this.hallgatok = hallgatok;
+    }
 }
