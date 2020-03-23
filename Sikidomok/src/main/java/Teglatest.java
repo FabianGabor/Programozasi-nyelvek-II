2f16d61a1d9b9e21a806128d20fd8d7b1ca9d8b4
Fábián Gábor <info@fabiangabor.com>
2020-03-23 15:50:18 +0100

    Teglatest terfogat & felszin

--- Sikidomok/src/main/java/Teglatest.java
+++ Sikidomok/src/main/java/Teglatest.java
@@ -4,7 +4,7 @@
  * 2020.
  */
 
-public class Teglatest extends Poliederek {
+public class Teglatest extends Poliederek implements TestInterface {
     private double aEl;
     private double bEl;
     private double cEl;
@@ -25,5 +25,57 @@ public class Teglatest extends Poliederek {
         this.setElnevezes(elnevezes);
     }
 
+    public double getaEl() {
+        return aEl;
+    }
 
+    public void setaEl(double aEl) {
+        this.aEl = aEl;
+    }
+
+    public double getbEl() {
+        return bEl;
+    }
+
+    public void setbEl(double bEl) {
+        this.bEl = bEl;
+    }
+
+    public double getcEl() {
+        return cEl;
+    }
+
+    public void setcEl(double cEl) {
+        this.cEl = cEl;
+    }
+
+    @Override
+    public double terfogat() {
+        return this.getaEl() * this.getbEl() * this.getcEl();
+    }
+
+    @Override
+    public double felszin() {
+        return 2 * ( this.getaEl() * this.getbEl() + this.getaEl() * this.getcEl() + this.getbEl() * this.getcEl() );
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer szoveg = new StringBuffer();
+        szoveg.append(this.getElnevezes() + " {\n");
+        szoveg.append("\t" + "a el = " + this.getaEl() + "\n");
+        szoveg.append("\t" + "b el = " + this.getbEl() + "\n");
+        szoveg.append("\t" + "c el = " + this.getcEl() + "\n");
+        szoveg.append("\t" + "tefogat = " + this.terfogat() + "\n");
+        szoveg.append("\t" + "felszin = " + this.felszin() + "\n");
+        szoveg.append("\t" + "Interface = " + kiir() + "\n");
+        szoveg.append("}\n");
+
+        return szoveg.toString();
+    }
+
+    public static void main(String[] args) {
+        Teglatest teglatest = new Teglatest("Teglatest", 1,2,3);
+        System.out.println(teglatest.toString());
+    }
 }
