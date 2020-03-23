6121186866debfc461a45550ce7b2549b715e6eb
Fábián Gábor <info@fabiangabor.com>
2020-03-23 10:12:46 +0100

    Megyjegyzesek

--- Neptun/src/main/java/Tombos.java
+++ Neptun/src/main/java/Tombos.java
@@ -7,15 +7,21 @@ public class Tombos {
         Arrays.fill(tomb, 0, 9, 3);
         Arrays.fill(tomb, 3, 6, 5);
         tomb[7] = 7;
+
         System.out.println(Arrays.toString(tomb));
+
+        // rendezes
         Arrays.sort(tomb);
         System.out.println(Arrays.toString(tomb));
-        int pos = Arrays.binarySearch(tomb, 7);
-        System.out.println("Index: " + pos);
+
+        // kereses
+        int key = 7;
+        int pos = Arrays.binarySearch(tomb, key);
+        System.out.println("Index of " + key + ": " + pos);
 
         int a = 5;
         Integer egesz = 5;
-        System.out.println(Integer.MAX_VALUE);
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
 
         String szo = "Alma";
         szo = "Alma es korte";
