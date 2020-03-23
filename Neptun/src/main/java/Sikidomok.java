80b5e539357b88e46cb43e783f2696ed639422be
Fábián Gábor <info@fabiangabor.com>
2020-03-23 11:09:22 +0100

    Sikidomok abstract

--- Neptun/src/main/java/Sikidomok.java
+++ Neptun/src/main/java/Sikidomok.java
@@ -3,4 +3,37 @@ abstract class Sikidomok {
 
     abstract double kerulet();
     abstract double terulet();
+
+    public Sikidomok(String elnevezes) {
+        this.elnevezes = elnevezes;
+    }
+
+    public String getElnevezes() {
+        return elnevezes;
+    }
+
+    public void setElnevezes(String elnevezes) {
+        this.elnevezes = elnevezes;
+    }
+
+    @Override
+    public String toString() {
+        return "Sikidom: " +
+                this.getElnevezes() + '\'';
+    }
+
+    public static void main(String[] args) {
+        Sikidomok alakzat;
+        alakzat = new Sikidomok("teglalap") {
+            @Override
+            double kerulet() {
+                return 0;
+            }
+
+            @Override
+            double terulet() {
+                return 0;
+            }
+        };
+    }
 }
