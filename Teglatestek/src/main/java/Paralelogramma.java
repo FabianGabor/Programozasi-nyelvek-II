7c28d73503707494b0a8a061b4db5ec3db9022d5
Fábián Gábor <info@fabiangabor.com>
2020-03-23 12:31:40 +0100

    Paralelogramma kerulet & terulet

--- Teglatestek/src/main/java/Paralelogramma.java
+++ Teglatestek/src/main/java/Paralelogramma.java
@@ -1,2 +1,36 @@
-public class Paralelogramma {
+public class Paralelogramma extends Negyszogek {
+    public Paralelogramma () {
+    }
+
+    public Paralelogramma(double aoldal, double boldal, double szog) {
+        this.setElnevezes("Paralelogramma");
+        this.setAoldal(aoldal);
+        this.setBoldal(boldal);
+        this.setSzog(szog);
+    }
+
+    @Override
+    public String toString() {
+        return this.getElnevezes() + "\n"
+                + "aoldal: " + this.getAoldal() + "\n"
+                + "boldal: " + this.getBoldal() + "\n"
+                + "szog: " + this.getSzog() + "\n"
+                + "kerulet: " + this.kerulet() + "\n"
+                + "terulet: " + this.terulet();
+    }
+
+    @Override
+    double kerulet() {
+        return 2 * (this.getAoldal() + this.getBoldal());
+    }
+
+    @Override
+    double terulet() {
+        return this.getAoldal() * this.getBoldal() * Math.sin(Math.toRadians(this.getSzog()));
+    }
+
+    public static void main (String[] args) {
+        Paralelogramma p = new Paralelogramma(4,5,45);
+        System.out.println(p.toString());
+    }
 }
