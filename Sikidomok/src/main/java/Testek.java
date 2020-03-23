22ce7c89602557708a948ab3678aa5934c6c3195
Fábián Gábor <info@fabiangabor.com>
2020-03-23 15:29:33 +0100

    Testek init

--- Sikidomok/src/main/java/Testek.java
+++ Sikidomok/src/main/java/Testek.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+/*
+ * Fábián Gábor
+ * CXNU8T
+ * 2020.
+ */
 
-public class Testek {
+abstract class Testek {
+    private String elnevezes;
+
+    public Testek() {}
+
+    public Testek(String elnevezes) {
+        this.elnevezes = elnevezes;
+    }
+
+    public String getElnevezes() {
+        return elnevezes;
+    }
+
+    public void setElnevezes(String elnevezes) {
+        this.elnevezes = elnevezes;
+    }
 }
