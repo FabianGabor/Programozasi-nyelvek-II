adde864f0b52e2564bb21dff24dc1764cd2aead4
Fábián Gábor <info@fabiangabor.com>
2020-03-24 13:21:54 +0100

    Gomb & henger letrehozva

--- Sikidomok/src/main/java/Nempoliederek.java
+++ Sikidomok/src/main/java/Nempoliederek.java
@@ -7,4 +7,12 @@
 abstract class Nempoliederek extends Testek {
     public Nempoliederek() {
     }
+
+    public static void main(String[] args) {
+        Gomb gomb = new Gomb("Gomb", 1);
+        System.out.println(gomb.toString());
+
+        Henger henger = new Henger("Henger", 1, 5);
+        System.out.println(henger.toString());
+    }
 }
