3a011a31279422236de9524d010651578b94649c
Fábián Gábor <info@fabiangabor.com>
2020-03-24 13:20:50 +0100

    Teglatest & Kocka letrehozva

--- Sikidomok/src/main/java/Poliederek.java
+++ Sikidomok/src/main/java/Poliederek.java
@@ -7,4 +7,12 @@
 abstract class Poliederek extends Testek {
     public Poliederek() {
     }
+
+    public static void main(String[] args) {
+        Teglatest teglatest = new Teglatest("Teglatest", 1,2,3);
+        System.out.println(teglatest.toString());
+
+        Kocka kocka = new Kocka("Kocka", 2);
+        System.out.println(kocka.toString());
+    }
 }
