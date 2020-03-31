7d3454f6df7d2648d756566695af59e83c73e54a
Fábián Gábor <info@fabiangabor.com>
2020-03-31 15:33:14 +0200

    Eltalalt szamok

--- Lotto/src/main/java/Lotto.java
+++ Lotto/src/main/java/Lotto.java
@@ -1,12 +1,30 @@
 public class Lotto {
     Szelveny szelveny = new Szelveny();
     Sorsolas sorsolas = new Sorsolas();
+    public int[] talalatok = new int[5];
+    public int talalatokSzama = 0;
 
     private void mainMenu() {
         Menu menu = new Menu();
         menu.addItem(new MenuItem("Szelveny kitoltes", this, "szelvenyKitoltes"));
         menu.addItem(new MenuItem("Lottohuzas", this, "lottohuzas"));
         menu.execute();
+    }
+
+    public int[] getTalalatok() {
+        return talalatok;
+    }
+
+    private void setTalalatok(int[] talalatok) {
+        this.talalatok = talalatok;
+    }
+
+    public int getTalalatokSzama() {
+        return talalatokSzama;
+    }
+
+    public void setTalalatokSzama(int talalatokSzama) {
+        this.talalatokSzama = talalatokSzama;
     }
 
     public void szelvenyKitoltes() {
@@ -16,23 +34,36 @@ public class Lotto {
     public void lottohuzas() {
         System.out.println(szelveny.toString());
 
-        sorsolas.sorsolas(10);
+        sorsolas.sorsolas(90);
         System.out.println(sorsolas.toString());
 
         int[] valasztottSzamok = szelveny.getValasztottSzamok();
         int[] talalatok = new int[5];
         int talalatokSzama = 0;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
             if (Szelveny.benneVan(sorsolas.getKihuzottSzamok(), valasztottSzamok[i])) {
                 talalatok[talalatokSzama] = valasztottSzamok[i];
                 talalatokSzama++;
             }
-        }
 
-        for (int i=0; i<talalatokSzama; i++)
-            System.out.println(talalatok[i]);
+        this.setTalalatok(talalatok);
+        this.setTalalatokSzama(talalatokSzama);
+
+        eltalaltSzamok();
     }
+
+    public void eltalaltSzamok() {
+        int talalatokSzama = getTalalatokSzama();
+        int[] talalatok = getTalalatok();
+
+        System.out.print(talalatokSzama + " talalat: ");
+        for (int i=0; i<talalatokSzama - 1; i++)
+            System.out.print(talalatok[i] + ", ");
+        if (talalatokSzama > 0)
+            System.out.println(talalatok[talalatokSzama - 1]);
+    }
+
 
     public void Kilepes() {
         System.exit(0);
