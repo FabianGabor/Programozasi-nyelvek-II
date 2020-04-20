f0bd014da54377c5ebf4313ff07d6fe7e39a05ce
Fábián Gábor <info@fabiangabor.com>
2020-04-20 14:23:56 +0200

    equals + compareTo

--- Kollekciok/src/main/java/Ember.java
+++ Kollekciok/src/main/java/Ember.java
@@ -1,15 +1,29 @@
 import java.io.Serializable;
 import java.util.Calendar;
 
-public class Ember implements Serializable {
+public class Ember implements Serializable, Comparable<Ember> {
     private String nev;
     private int szuletesiEv;
     private boolean nem;
     private int jovedelem;
 
+    public Ember() {
+        this.nev = "Teszt Elek";
+        this.szuletesiEv = 1900;
+        this.nem = true;
+        this.jovedelem = 1000000;
+    }
+
     public Ember(String nev, int szuletesiEv) {
         this.nev = nev;
         this.szuletesiEv = szuletesiEv;
+    }
+
+    public Ember(String nev, int szuletesiEv, boolean nem, int jovedelem) {
+        this.nev = nev;
+        this.szuletesiEv = szuletesiEv;
+        this.nem = nem;
+        this.jovedelem = jovedelem;
     }
 
     public int eletkor() {
@@ -51,8 +65,44 @@ public class Ember implements Serializable {
         this.jovedelem = jovedelem;
     }
 
+    @Override
+    public String toString() {
+        return "Ember{" +
+                "nev='" + nev + '\'' +
+                ", szuletesiEv=" + szuletesiEv +
+                ", nem=" + (nem?"ferfi":"no") +
+                ", jovedelem=" + jovedelem +
+                '}';
+    }
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ember ember = (Ember) o;
+        return szuletesiEv == ember.szuletesiEv &&
+                nem == ember.nem &&
+                jovedelem == ember.jovedelem &&
+                nev.equals(ember.nev);
+    }
+*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ember)) return false;
+        Ember ember = (Ember) o;
+        return getSzuletesiEv() == ember.getSzuletesiEv() &&
+                getNev().equals(ember.getNev());
+    }
+
     public static void main(String[] args) {
         Ember ember = new Ember("Teszt", 1987);
         System.out.println(ember.eletkor());
+    }
+
+    @Override
+    public int compareTo(Ember e) {
+        return this.getNev().compareTo(e.getNev());
     }
 }
