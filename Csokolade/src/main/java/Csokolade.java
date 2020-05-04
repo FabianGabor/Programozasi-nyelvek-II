2cc0ae334548d99a1b66d0e91b87b01ec24ca587
Fábián Gábor <info@fabiangabor.com>
2020-05-04 10:53:44 +0200

    implements CsokiInterface, Comparable

--- Csokolade/src/main/java/Csokolade.java
+++ Csokolade/src/main/java/Csokolade.java
@@ -1,4 +1,19 @@
-public class Csokolade {
+public class Csokolade implements CsokiInterface, Comparable<Csokolade> {
+	@Override
+	public void aremeles(int osszeg) {
+		this.setAr(this.getAr() + osszeg);
+	}
+
+	@Override
+	public int akcio(int szazalek) {
+		return (int) (this.getAr() * (1 - szazalek / 100.0));
+	}
+
+	@Override
+	public int compareTo(Csokolade csokolade) {
+		return this.getAr() - csokolade.getAr();
+	}
+
 	public enum Tipus {
 		TEJ, ÉT, FEHÉR;
 
@@ -13,9 +28,81 @@ public class Csokolade {
 				default:
 					return 0;
 			}
-
 		}
+	}
 
+	private String nev;
+	private Tipus csokitipus;
+	private int tomeg;
+	private String toltelek;
+	private int ar;
+
+	public Csokolade(String nev, Tipus csokitipus, int tomeg, String toltelek, int ar) {
+		this.nev = nev;
+		this.csokitipus = csokitipus;
+		this.tomeg = tomeg;
+		this.toltelek = toltelek;
+		this.ar = ar;
+	}
+
+	public String getNev() {
+		return nev;
+	}
+
+	public void setNev(String nev) {
+		this.nev = nev;
+	}
+
+	public Tipus getCsokitipus() {
+		return csokitipus;
+	}
+
+	public void setCsokitipus(Tipus csokitipus) {
+		this.csokitipus = csokitipus;
+	}
+
+	public int getTomeg() {
+		return tomeg;
+	}
+
+	public void setTomeg(int tomeg) {
+		this.tomeg = tomeg;
+	}
+
+	public String getToltelek() {
+		return toltelek;
+	}
+
+	public void setToltelek(String toltelek) {
+		this.toltelek = toltelek;
+	}
+
+	public int getAr() {
+		return ar;
+	}
+
+	public void setAr(int ar) {
+		this.ar = ar;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Csokolade)) return false;
+		Csokolade csokolade = (Csokolade) o;
+		return getNev().equals(csokolade.getNev()) &&
+				getCsokitipus() == csokolade.getCsokitipus();
+	}
+
+	@Override
+	public String toString() {
+		return "Csokolade{" +
+				"nev='" + nev + '\'' +
+				", csokitipus=" + csokitipus +
+				", tomeg=" + tomeg +
+				", toltelek='" + toltelek + '\'' +
+				", ar=" + ar +
+				'}';
 	}
 
 	public static void main(String[] args) {
