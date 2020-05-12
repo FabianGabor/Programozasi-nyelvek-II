8362d3ec640b03addb4cb2634e76f7fd5b052136
Fábián Gábor <info@fabiangabor.com>
2020-05-12 18:33:02 +0200

    Bolt HDD Constructor-ok egyszerusitese

--- Szamitastechnikai bolt/src/main/java/HDD.java
+++ Szamitastechnikai bolt/src/main/java/HDD.java
@@ -2,32 +2,19 @@ public class HDD extends Hardware {
 	// "Winchester" is a code-name for an HDD introduced by IBM in 1973
 	int kapacitas;
 	int fordulatszam;
-	int buffer;
 	double eleresiIdo;
 
-	public HDD(int cikkszam, String megnevezes, int beszerzesiAr, int kapacitas) {
-		super(cikkszam, megnevezes, beszerzesiAr);
-		this.kapacitas = kapacitas;
+	public HDD() {
 	}
 
-	public HDD(int cikkszam, String megnevezes, int beszerzesiAr, int kapacitas, int fordulatszam) {
+	public HDD(int cikkszam, String megnevezes, int beszerzesiAr) {
+		super(cikkszam, megnevezes, beszerzesiAr);
+	}
+
+	public HDD(int cikkszam, String megnevezes, int beszerzesiAr, int kapacitas, int fordulatszam, double eleresiIdo) {
 		super(cikkszam, megnevezes, beszerzesiAr);
 		this.kapacitas = kapacitas;
 		this.fordulatszam = fordulatszam;
-	}
-
-	public HDD(int cikkszam, String megnevezes, int beszerzesiAr, int kapacitas, int fordulatszam, int buffer) {
-		super(cikkszam, megnevezes, beszerzesiAr);
-		this.kapacitas = kapacitas;
-		this.fordulatszam = fordulatszam;
-		this.buffer = buffer;
-	}
-
-	public HDD(int cikkszam, String megnevezes, int beszerzesiAr, int kapacitas, int fordulatszam, int buffer, double eleresiIdo) {
-		super(cikkszam, megnevezes, beszerzesiAr);
-		this.kapacitas = kapacitas;
-		this.fordulatszam = fordulatszam;
-		this.buffer = buffer;
 		this.eleresiIdo = eleresiIdo;
 	}
 
@@ -47,19 +34,23 @@ public class HDD extends Hardware {
 		this.fordulatszam = fordulatszam;
 	}
 
-	public int getBuffer() {
-		return buffer;
-	}
-
-	public void setBuffer(int buffer) {
-		this.buffer = buffer;
-	}
-
 	public double getEleresiIdo() {
 		return eleresiIdo;
 	}
 
 	public void setEleresiIdo(double eleresiIdo) {
 		this.eleresiIdo = eleresiIdo;
+	}
+
+	@Override
+	public String toString() {
+		return "HDD" +
+				"\n\tcikkszam:\t\t" + cikkszam +
+				"\n\tmegnevezes:\t\t" + megnevezes +
+				"\n\tbeszerzesiAr:\t" + beszerzesiAr +
+				"\n\tkapacitas:\t\t" + kapacitas +
+				"\n\tfordulatszam:\t" + fordulatszam +
+				"\n\teleresiIdo:\t\t" + eleresiIdo +
+				"\n";
 	}
 }
