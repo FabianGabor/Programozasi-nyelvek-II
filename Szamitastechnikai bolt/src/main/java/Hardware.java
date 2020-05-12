7b136d009ddc01f896f7200752cdc8f40ed75e19
Fábián Gábor <info@fabiangabor.com>
2020-05-12 15:50:13 +0200

    Hardware class

---
+++ Szamitastechnikai bolt/src/main/java/Hardware.java
@@ -0,0 +1,54 @@
+public class Hardware implements HardwareInterface {
+	int cikkszam;
+	String megnevezes;
+	int beszerzesiAr;
+	static double haszonkulcs;
+
+	public Hardware(int cikkszam, String megnevezes, int beszerzesiAr) {
+		this.cikkszam = cikkszam;
+		this.megnevezes = megnevezes;
+		this.beszerzesiAr = beszerzesiAr;
+	}
+
+	public int getCikkszam() {
+		return cikkszam;
+	}
+
+	public void setCikkszam(int cikkszam) {
+		this.cikkszam = cikkszam;
+	}
+
+	public String getMegnevezes() {
+		return megnevezes;
+	}
+
+	public void setMegnevezes(String megnevezes) {
+		this.megnevezes = megnevezes;
+	}
+
+	public int getBeszerzesiAr() {
+		return beszerzesiAr;
+	}
+
+	public void setBeszerzesiAr(int beszerzesiAr) {
+		this.beszerzesiAr = beszerzesiAr;
+	}
+
+	public static double getHaszonkulcs() {
+		return haszonkulcs;
+	}
+
+	public static void setHaszonkulcs(double haszonkulcs) {
+		Hardware.haszonkulcs = haszonkulcs;
+	}
+
+	@Override
+	public int geteladasiar() {
+		return (int) (this.getBeszerzesiAr() * (1 + getHaszonkulcs()));
+	}
+
+	@Override
+	public int haszon() {
+		return (int) (this.getBeszerzesiAr() * getHaszonkulcs());
+	}
+}
